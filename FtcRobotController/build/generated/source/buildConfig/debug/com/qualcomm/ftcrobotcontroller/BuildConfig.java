3a0ed9aa32addca4f4df8c7308eaa2d8d42bb27f
vvasa33 <vvasa33@icloud.com>
2022-02-15 18:21:43 -0500

    changed locations of files for better readability

---
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.qualcomm.ftcrobotcontroller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 44;
+  public static final String VERSION_NAME = "7.1";
+  // Fields from default config.
+  public static final String APP_BUILD_TIME = "2022-02-15T16:18:48.968-0500";
+}
